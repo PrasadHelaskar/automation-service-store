4464c9298e6e831ac782acd562a569f1976c0cd9
Prasad-Omnify <prasad@getomnify.com>
2024-12-16 11:27:11 +0530

    added camp booking flow

---
+++ pages/camp_booking.py
@@ -0,0 +1,74 @@
+from tests.base_page import BasePage
+from selenium.webdriver.common.by import By
+from Base.logfile import Logger
+
+log=Logger().get_logger()
+
+class camp_booking(BasePage):
+    __private_camp_page=(By.CSS_SELECTOR ,"a[href='/camps?b=t']")
+    service=1
+    __private_camp_selection=(By.XPATH, f"(//a[@class='primary-button-card bc4 fc1'])[{service}]")
+    __private_add_attendee=(By.ID, "addCampAttendeeBtn")
+    def select_attendee(self , i):
+        xpath=f"(//div[@class='checkbox w-checkbox'])[{i}]"
+        __private_attendee_select=(By.XPATH, xpath)
+        return __private_attendee_select
+    __private_attendee_proced=(By.XPATH, "(//div[@class='fc3 booking-footer-button-text-left attendee-form-done button-text-medium-regular'])[1]")
+    def select_schedule(self, i):
+        xpath=f"(//input[@class='w-checkbox-input class-schedule-check'])[{i}]"
+        __private_schedule_select=(By.XPATH, xpath)
+        return __private_schedule_select
+    __private_schedule_proceed=(By.ID,"class_go_next_btn")
+    __private_addonpage=(By.XPATH,"//div[@class='add-on-section bc4_a bw1t bottom-120']")
+    __private_Addon_Proceed=(By.XPATH, "//button[@class='discount-button bc4 shrink w-button']")
+    __private_Waiver_Checkbox=(By.ID, "waiverCheckbox")
+    __private_Review_Proceed_cardno=(By.ID, "submitFinalReviewFormBtn")
+    __private_Review_Proceed_card=(By.CSS_SELECTOR,"div[class='fc3 booking-footer-button-text-right body-text-2-medium']")
+    __private_HOME_BUTTON= (By.LINK_TEXT, "BOOK ANOTHER")
+
+
+    def click_camp_page(self):
+        self.click(self.__private_camp_page)
+
+    def click_camp_selection(self):
+        self.click(self.__private_camp_selection)
+
+    def click_add_attendee(self):
+        self.click(self.__private_add_attendee)
+
+    def click_attendee(self,i):
+        self.click(self.select_attendee(i))
+
+    def click_attendee_proceed(self):
+        self.click(self.__private_attendee_proced)
+
+    def click_schedule(self,i):
+        self.click(self.select_schedule(i))
+
+    def click_schedule_proceed(self):
+        self.click(self.__private_schedule_proceed)
+
+    def visible_addon_page(self):
+        try:
+            op=self.is_visible(self.__private_addonpage)
+            log.info("add on page check:"+str(op))   
+            return op
+        except:
+            return False
+
+    def click_addon_proceed(self):
+        self.click(self.__private_Addon_Proceed)
+
+    def click_waiverbox(self):
+        self.click(self.__private_Waiver_Checkbox)
+
+    def click_review_proceed(self):
+        try:    
+            value=self.is_visible(self.__private_Review_Proceed_cardno)
+        except:
+            value=False
+        locator=(self.__private_Review_Proceed_cardno) if value else (self.__private_Review_Proceed_card)
+        self.click(locator)
+
+    def click_home(self):
+        self.click(self.__private_HOME_BUTTON)
---
+++ tests/test_camp.py
@@ -0,0 +1,43 @@
+import time
+import traceback
+from selenium import webdriver
+import pytest
+from Base.logfile import Logger
+from Base.stripe_popup import stripe_action
+from pages.camp_booking import camp_booking
+from tests.login import loginAction
+from Base.random_select import select_random
+
+log=Logger().get_logger()
+
+class TestCamp_booking():
+    @pytest.mark.order(9)
+    def test_camp_booking(self, driver):    
+        cmp=camp_booking(driver)
+        loginAction().login_action(driver)
+        cmp.click_camp_page()
+        time.sleep(5)
+        cmp.click_camp_selection()
+        cmp.click_add_attendee()
+        time.sleep(2)
+        script = """return document.getElementsByClassName('w-checkbox-input waitlist-checkbox').length;"""
+        i= driver.execute_script(script)
+        log.info("element count="+str(i))
+        attendee_count=select_random().random_number(i)
+        log.info("selected attendee count="+str(attendee_count))
+        for j in range (1, (attendee_count+1)):
+            cmp.click_attendee(j)
+        cmp.click_attendee_proceed()
+        schedule_count=5
+        for j in range(1, schedule_count):
+            cmp.click_schedule(j)
+        cmp.click_schedule_proceed()
+        if cmp.visible_addon_page():
+            cmp.click_addon_proceed()
+        cmp.click_waiverbox()
+        cmp.click_review_proceed()
+        stripe_action().stripe_data_enty(driver)
+        driver.execute_script("window.debugger = function() {};")
+        cmp.click_home()
+        loginAction().authenticate_cookie(driver)
+        log.info("Camp booking Compleated")        
