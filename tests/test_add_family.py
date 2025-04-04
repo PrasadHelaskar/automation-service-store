be568ed8fd1c3ebaec089f5b12a8dae133986bd2
Prasad-Omnify <prasad@getomnify.com>
2025-04-04 11:10:38 +0530

    Update date in data.json, add Faker for random data generation in tests, and enhance logging format

--- tests/test_add_family.py
+++ tests/test_add_family.py
@@ -1,5 +1,6 @@
 import time
 import pytest
+from faker import Faker
 from base.logfile import Logger
 from pages.addfamily_Client_profile import addfamily
 from tests.login import loginAction
@@ -12,9 +13,10 @@ class Test_add_family():
     @pytest.mark.sign_up
     @pytest.mark.order(2)
     def test_add_family_client(self, driver):
+        """Method to add the family member from the client profile"""
         loginAction().login_action(driver)
         time.sleep(5)
-        count=8
+        count=7
         af=addfamily(driver)
         af.click_profile()
         af.click_profile_page()
@@ -23,9 +25,9 @@ class Test_add_family():
             time.sleep(5)
             start_time=time.time()
             af.click_add_family_btn()
-            fn=select_random().first_name()
+            fn=Faker().first_name()
             af.type_first_name(fn)
-            ln=select_random().last_name()
+            ln=Faker().last_name()
             af.type_last_name(ln)
             af.type_dob(23)
             af.type_dob(7)
@@ -34,14 +36,14 @@ class Test_add_family():
             af.click_submit_button()
             script = """return document.getElementsByClassName('body-text-1-medium family-details-textblock1').length;"""
             i= driver.execute_script(script)
-            log.info("element count="+str(i))
+            log.info("element count= %s",str(i))
             time.sleep(2)
             div=af.scroll_div()
             driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight;",div)
             value=True if (af.get_added_name(i)==(fn+" "+ln)) else False
             log.info("Completed the family addition Process")
             end_time=time.time()
-            log.info("Execution time: "+str(end_time-start_time))
+            log.info("Execution time: %s",str(end_time-start_time))
             # if value:
             #     af.click_back_button()
             #     log.info("Back to home with adding the family member")
--- tests/test_signup.py
+++ tests/test_signup.py
@@ -1,12 +1,12 @@
 import pytest
+import time
+import os
+from faker import Faker
+from dotenv import load_dotenv
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
 from base.revised_API_LOG import APILOG
 from pages.signup import signup
-from dotenv import load_dotenv
-from selenium.webdriver.common.by import By
-import os
-import time
-from base.random_select import select_random
-from selenium.webdriver.support.ui import Select
 from tests.login import loginAction
 
 class Test_sign_up():
@@ -15,29 +15,28 @@ class Test_sign_up():
     @pytest.mark.order(1)
     def test_sign_up(self, driver):
         load_dotenv()
-        sr=select_random()
         # apilogger = APILOG(driver)
         driver.get(os.getenv('URL'))
         sg=signup(driver)
         sg.click_signup()
         sg.enter_email(os.getenv('EMAIL'))
         sg.click_continuebutton()
-        sg.enter_firstname(sr.first_name())
-        sg.enter_lastname(sr.last_name())
-
-        if (sg.check_custom_field()):
-            sg.click_dobfield()
-            time.sleep(5)
-            month_dropdown = driver.find_element(By.NAME, "months")
-            select_month = Select(month_dropdown)
-            month=sr.random_month()
-            select_month.select_by_visible_text(month)
-            year_dropdown = driver.find_element(By.NAME, "years")
-            select_year = Select(year_dropdown)
-            year=sr.random_year()
-            select_year.select_by_visible_text(str(year))
-            sg.click_date()
-            sg.click_dobfield()
+        sg.enter_firstname(Faker().first_name())
+        sg.enter_lastname(Faker().last_name())
+        
+        # if (sg.check_custom_field()):
+        #     sg.click_dobfield()
+        #     time.sleep(5)
+        #     month_dropdown = driver.find_element(By.NAME, "months")
+        #     select_month = Select(month_dropdown)
+        #     month=sr.random_month()
+        #     select_month.select_by_visible_text(month)
+        #     year_dropdown = driver.find_element(By.NAME, "years")
+        #     select_year = Select(year_dropdown)
+        #     year=sr.random_year()
+        #     select_year.select_by_visible_text(str(year))
+        #     sg.click_date()
+        #     sg.click_dobfield()
             
         sg.click_submit()
         sg.enter_password(os.getenv('PASSWORD'))
