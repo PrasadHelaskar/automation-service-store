188ad4d2363173386d19bf2eb2bf8b2c5dd4829a
Prasad-Omnify <prasad@getomnify.com>
2025-05-26 22:51:15 +0530

    Refactor test cases in test_program_age, test_trial, and test_signup for improved clarity; add new test for non-renewing classpack functionality

--- tests/subscriptions/test_program_age.py
+++ tests/subscriptions/test_program_age.py
@@ -64,7 +64,7 @@ class Test_program_age():
         pb.click_proceed()
         time.sleep(3)
         
-        add_family_checkout_flow(driver)
+        # add_family_checkout_flow(driver)
  
         script="""return document.getElementsByClassName("w-checkbox-input waitlist-checkbox").length;"""
         recived_count=driver.execute_script(script)
